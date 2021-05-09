48614879d85f941cdaa2f1d82d53ac08224034b5
Anton Berestov <8berestov8@gmail.com>
2021-05-09 22:27:30 +0400

    create app truckparts

---
+++ src/views/Admin.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-app app>
+    <v-navigation-drawer app v-model="drawer">
+    </v-navigation-drawer>
+
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+    </v-app-bar>
+
+    <v-main>
+      <v-container fluid>
+        CONTENT
+      </v-container>
+    </v-main>
+
+    <v-footer app class="footer">
+      <h3>© 2021 TruckParts. All rights reserved.</h3>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  data: () => ({
+    drawer: true
+  }),
+  components: {},
+}
+</script>
+
+<style scoped>
+.footer {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
