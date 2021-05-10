4451c56c0efc5f71d6b58efdc6c691bde18f0ed3
Anton Berestov <8berestov8@gmail.com>
2021-05-10 07:26:13 +0400

    update footer

--- src/views/admin/Home.vue
+++ src/views/admin/Home.vue
@@ -1,6 +1,63 @@
 <template>
   <v-app app>
     <v-navigation-drawer app v-model="drawer">
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title">
+            Truckparts
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            интернет магазин запчастей
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list
+        dense
+        nav
+      >
+        <v-list-item>
+          <v-list-item-icon>
+            <v-icon>cdsfdf</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>Система</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item>
+          <v-list-item-icon>
+            <v-icon>cdsfdf</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>Магазин</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item>
+          <v-list-item-icon>
+            <v-icon>cdsfdf</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>Содержимое</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item>
+          <v-list-item-icon>
+            <v-icon>cdsfdf</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>Пользователи</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
     </v-navigation-drawer>
 
     <v-app-bar app>
@@ -21,7 +78,12 @@
     </v-main>
 
     <v-footer app class="footer">
-      <v-footer-title>© 2021 TruckParts. All rights reserved.</v-footer-title>
+      <v-col
+        class="text-center"
+        cols="12"
+      >
+        <strong> © {{ new Date().getFullYear() }} — TruckParts. All rights reserved.</strong>
+      </v-col>
     </v-footer>
   </v-app>
 </template>
